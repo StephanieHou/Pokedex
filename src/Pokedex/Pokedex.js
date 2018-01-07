7fbd0f9e357a04aaf00e510001cb932138f6fc3c
StephanieHou <32886693+StephanieHou@users.noreply.github.com>
2018-01-06 22:59:34 -0500

    CSS Changes

--- src/Pokedex/Pokedex.js
+++ src/Pokedex/Pokedex.js
@@ -9,27 +9,40 @@ const Pokedex = () => (
       alt="poke"
       border="0"
     />
+    <img
+      id="sprite"
+      src="https://vignette.wikia.nocookie.net/pokemon/images/8/81/Bulbasaur_XY_EffectAnimation_Sprite.gif/revision/latest?cb=20141114181826"
+      alt="sprite"
+    />
     <div id="pokeinfo">
-    <h1> Bulbasaur #001 </h1>
-    <p id="des">
-      {" "}
-      Bulbasaur can be seen napping in bright sunlight. There is a seed on its
-      back. By soaking up the sun's rays, the seed grows progressively larger.{" "}
-    </p>
-    <table align="center" text-align ="left">
-    <tr>
-     <td><p id="height"> Height: 2ft 04in </p></td>
-     <td><p id="weight"> Weight: 15.2 lbs</p></td>
-    </tr>
-    <tr>
-     <td><p id="category"> Category: Seed </p></td>
-     <td><p id="abil"> Abilities: Overgrown</p></td>
-    </tr>
-    </table>
-    <p id="type"> Type: Grass Poison</p>
-    <p id="weak"> Weaknesses: Fire Flying Ice Psychic</p>
+      <h1> Bulbasaur #001 </h1>
+      <p id="des">
+        {" "}
+        Bulbasaur can be seen napping in bright sunlight. There is a seed on its
+        back. By soaking up the sun's rays, the seed grows progressively larger.{" "}
+      </p>
+      <table align="center" text-align="left">
+        <tr>
+          <td>
+            <p id="height"> Height: 2ft 04in </p>
+          </td>
+          <td>
+            <p id="weight"> Weight: 15.2 lbs</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p id="category"> Category: Seed </p>
+          </td>
+          <td>
+            <p id="abil"> Abilities: Overgrown</p>
+          </td>
+        </tr>
+      </table>
+      <p id="type"> Type: Grass Poison</p>
+      <p id="weak"> Weaknesses: Fire Flying Ice Psychic</p>
     </div>
-    <div id="bottombarpoke" />
+    <div id="bottombarpoke"/>
   </div>
 );
 
